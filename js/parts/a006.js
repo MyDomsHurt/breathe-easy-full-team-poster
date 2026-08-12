616bc79470d81188452f438ad8c13d3747a86e22
MyDomsHurt <71425449+MyDomsHurt@users.noreply.github.com>
2026-08-12 12:26:16 +0800

    Add asset assemblers a006 and a007

---
+++ js/parts/a006.js
@@ -0,0 +1,5 @@
+import { C6_0 } from "./c006_0.js";
+import { C6_1 } from "./c006_1.js";
+export const A6 = {
+  "assets/logos/wordmark.png": "data:image/png;base64," + C6_0 + C6_1
+};
---
+++ js/parts/a007.js
@@ -0,0 +1,5 @@
+import { C7_0 } from "./c007_0.js";
+import { C7_1 } from "./c007_1.js";
+export const A7 = {
+  "assets/numbers/digits/0.png": "data:image/png;base64," + C7_0 + C7_1
+};
