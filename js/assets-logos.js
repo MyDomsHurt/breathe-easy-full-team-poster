b5bd338db03ed6f2313d6e7b7bb3b00bb17b94b3
MyDomsHurt <71425449+MyDomsHurt@users.noreply.github.com>
2026-08-12 13:11:51 +0800

    Fix assets-logos.js assembler (replace path stub)

--- js/assets-logos.js
+++ js/assets-logos.js
@@ -1,1 +1,10 @@
-@file:///Users/jefflamb/breathe-easy-poster/js/assets-logos.js
+/* Chunked runtime assets: assets-logos */
+import { ASSETS_LOGOS_C0 } from "./assets-logos_c0.js";
+import { ASSETS_LOGOS_C1 } from "./assets-logos_c1.js";
+import { ASSETS_LOGOS_C2 } from "./assets-logos_c2.js";
+import { ASSETS_LOGOS_C3 } from "./assets-logos_c3.js";
+import { ASSETS_LOGOS_C4 } from "./assets-logos_c4.js";
+export const ASSETS_LOGOS = {
+  "assets/logos/skyline-badge.png": "data:image/png;base64," + ASSETS_LOGOS_C0 + ASSETS_LOGOS_C1,
+  "assets/logos/wordmark.png": "data:image/png;base64," + ASSETS_LOGOS_C2 + ASSETS_LOGOS_C3 + ASSETS_LOGOS_C4,
+};
