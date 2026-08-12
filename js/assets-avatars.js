682774976d2dc7b387e3919981427ff777bfc25e
MyDomsHurt <71425449+MyDomsHurt@users.noreply.github.com>
2026-08-12 13:18:05 +0800

    Add embedded asset chunk batch 00 (4 files)

---
+++ js/assets-avatars.js
@@ -0,0 +1,18 @@
+/* Chunked runtime assets: assets-avatars */
+import { ASSETS_AVATARS_C0 } from "./assets-avatars_c0.js";
+import { ASSETS_AVATARS_C1 } from "./assets-avatars_c1.js";
+import { ASSETS_AVATARS_C2 } from "./assets-avatars_c2.js";
+import { ASSETS_AVATARS_C3 } from "./assets-avatars_c3.js";
+import { ASSETS_AVATARS_C4 } from "./assets-avatars_c4.js";
+import { ASSETS_AVATARS_C5 } from "./assets-avatars_c5.js";
+import { ASSETS_AVATARS_C6 } from "./assets-avatars_c6.js";
+import { ASSETS_AVATARS_C7 } from "./assets-avatars_c7.js";
+import { ASSETS_AVATARS_C8 } from "./assets-avatars_c8.js";
+import { ASSETS_AVATARS_C9 } from "./assets-avatars_c9.js";
+export const ASSETS_AVATARS = {
+  "assets/avatars/tiago.png": "data:image/png;base64," + ASSETS_AVATARS_C0 + ASSETS_AVATARS_C1,
+  "assets/avatars/matthew.png": "data:image/png;base64," + ASSETS_AVATARS_C2 + ASSETS_AVATARS_C3,
+  "assets/avatars/iggi.png": "data:image/png;base64," + ASSETS_AVATARS_C4 + ASSETS_AVATARS_C5,
+  "assets/avatars/alun.png": "data:image/png;base64," + ASSETS_AVATARS_C6 + ASSETS_AVATARS_C7,
+  "assets/avatars/nick.png": "data:image/png;base64," + ASSETS_AVATARS_C8 + ASSETS_AVATARS_C9,
+};
